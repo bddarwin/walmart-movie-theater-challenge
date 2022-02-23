e1afeff348a8c4ad6170b178146c50d6d56a02ea
Benjamin Darwin <benjamin.darwin@snhu.edu>
2022-02-22 23:18:24 -0500

    Updating tests file to test using main function, instead of calling functions individually.

--- MovieTheaterTests.py
+++ MovieTheaterTests.py
@@ -2,10 +2,10 @@ import MovieTheater
 
 #Test 1 - Opening an Invalid File.
 print("Test One - Opening an Invalid File.\n")
-MovieTheater.get_input_file("SampleData/SampleDataFAIL.txt")
+MovieTheater.movieTheater("SampleData/SampleDataFAIL.txt")
 print("\n")
 
 #Test 2 - Opening a valid file.
 print("Test Two - Opening a Valid File.\n")
-MovieTheater.get_input_file("SampleData/SampleDataOne.txt")
+MovieTheater.movieTheater("SampleData/SampleDataOne.txt")
 print("\n")
